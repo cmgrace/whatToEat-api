e905b9f6c78d6456bf2f7b90a86d29885cbca8cd
Ming Cheng <cmgrace@bu.edu>
2021-03-31 15:21:21 -0400

    first commit

---
+++ seeds/trunct.restaurants.sql
@@ -0,0 +1,5 @@
+TRUNCATE
+    restaurant_detail,
+  whattoeat_restaurants
+  
+  RESTART IDENTITY CASCADE;
---
+++ src/config.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+module.exports = {
+  PORT: process.env.PORT || 8000,
+  NODE_ENV: process.env.NODE_ENV || "development",
+  DATABASE_URL:
+    process.env.DATABASE_URL ||
+    "postgresql://dunder_mifflin@localhost/whattoeat",
+  TEST_DATABASE_URL:
+    process.env.TEST_DATABASE_URL ||
+    "postgresql://dunder_mifflin@localhost/whattoeat-test",
+  JWT_SECRET: process.env.JWT_SECRET || "change-this-secret",
+};
